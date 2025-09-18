454bea43cdf1afbaf4c248d5f58f28ca5628a420
Chethan M N <mnchethan9900@gmail.com>
2025-09-18 20:52:13 +0530

    SetUP-Home-SelectComponent

---
+++ front-end/src/Components/RadioComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Css/RadioComponent.css'
+
+
+const RadioComponent = ({text}) => {
+    return (
+        <div className='form-check-label'>
+            <span className='text'>{text}</span>
+        </div>
+    );
+};
+
+export default RadioComponent;
