a8704a27cfac02ab18eef8f46d2960ab171bed5e
Chethan M N <mnchethan9900@gmail.com>
2025-09-18 20:57:09 +0530

    SetUP-Home-SelectComponent

--- front-end/src/Components/RadioComponent.js
+++ front-end/src/Components/RadioComponent.js
@@ -4,6 +4,10 @@ import '../Css/RadioComponent.css'
 
 const RadioComponent = ({text}) => {
     return (
+        // This component represents a single radio button option.
+        // It receives a 'text' prop which is displayed next to the radio button.
+        // The structure includes a div with class 'form-check-label' for styling,
+        // and a span with class 'text' to hold the actual text content.
         <div className='form-check-label'>
             <span className='text'>{text}</span>
         </div>
